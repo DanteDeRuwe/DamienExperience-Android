a63a0e11f029e062384e14d365164c30d3e009ba
Jordy <jordy.vankerkvoorde@student.hogent.be>
2020-11-10 09:43:19 +0100

    temporary bugfix
    needs to be undone when startwalk in backend is finished

--- app/src/main/java/com/example/damiantour/startRoute/StartRouteSuccess.kt
+++ app/src/main/java/com/example/damiantour/startRoute/StartRouteSuccess.kt
@@ -63,7 +63,7 @@ class StartRouteSuccess : Fragment() {
     private fun initCountdown(){
         //natuurlijk moet de Future nog ingesteld worden op het verschil van de starttijd van de wandeling en de huidige datum (in millis)
         //aangezien dat niet zo tof gaat zijn voor te testen zou ik het gewoon zo hardcoded laten staan en uitleggen aan de klant
-        val timer = object: CountDownTimer(80000, 1000) {
+        val timer = object: CountDownTimer(20000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 binding.daysTV.setText(Math.floor((millisUntilFinished/ (1000 * 60 * 60 * 24)).toDouble()).toInt().toString())
                 binding.hoursTV.setText(Math.floor(((millisUntilFinished % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toDouble()).toInt().toString())
@@ -75,7 +75,8 @@ class StartRouteSuccess : Fragment() {
                 toggleButton(true)
                 lifecycleScope.launch{
                     val token = preferences.getString("TOKEN", null).toString()
-                    apiService.startWalk(token)
+                    //onderstaande gooit error omdat het een jsonobject verwacht. Nog te zien wat we in BE gaan doen
+                    //apiService.startWalk(token)
                 }
             }
         }
