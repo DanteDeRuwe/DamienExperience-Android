3f200d954b9510e6bd92a23e6c8c1204c49b6a29
Jonas Haenebalcke <jonashaenebalcke@hotmail.be>
2020-12-09 17:48:07 +0100

    Add QR Reader

--- app/src/main/java/com/example/damiantour/MainActivity.kt
+++ app/src/main/java/com/example/damiantour/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.damiantour
+
+import android.R.attr
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.damiantour.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_qr_code.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +21,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //set the view of the activity
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    fun scanQr() {
+        val scanner = IntentIntegrator(this)
+
+        scanner.initiateScan();
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if( resultCode == Activity.RESULT_OK){
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            if (result != null) {
+                if (result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
     }
 }
