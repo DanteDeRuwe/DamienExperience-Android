bec54ea84f33201b4d3ab9616a28b6ee6794be3c
Jordy <jordy.vankerkvoorde@student.hogent.be>
2020-12-19 15:37:28 +0100

    Add logout to screens fixed #18

--- app/src/main/java/com/example/damiantour/startRoute/StartRouteNotRegisteredFragment.kt
+++ app/src/main/java/com/example/damiantour/startRoute/StartRouteNotRegisteredFragment.kt
@@ -1,7 +1,9 @@
 package com.example.damiantour.startRoute
 
 import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.damiantour.R
 import com.example.damiantour.databinding.FragmentStartRouteNotRegisteredBinding
 
@@ -20,15 +23,21 @@ class StartRouteNotRegisteredFragment : Fragment() {
 
     private lateinit var binding: FragmentStartRouteNotRegisteredBinding
 
+    private lateinit var preferences: SharedPreferences
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        preferences = requireActivity().getSharedPreferences("damian-tours", Context.MODE_PRIVATE)
         binding = FragmentStartRouteNotRegisteredBinding.inflate(inflater, container, false)
         binding.registerButton.setOnClickListener(View.OnClickListener {
             navigateToWeb()
         })
+
+        binding.logoutButtnSRNR.setOnClickListener {
+            logout()
+        }
 
         return binding.root
     }
@@ -42,6 +51,12 @@ class StartRouteNotRegisteredFragment : Fragment() {
             startActivity(intent)
         }
     }
+
+    private fun logout(){
+        preferences.edit().remove("TOKEN").apply()
+        view?.findNavController()?.navigate(R.id.action_startRouteNotRegistered_to_loginFragment)
+    }
+
 
 
 }
